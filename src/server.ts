f4c729a1a6c42fb16d42321f2447cc8076e0deee
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-05-12 20:26:36 +0330

    Add server setup with environment configuration and database connection

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./app";
+import { Express } from "express";
+import connectToDatabase from "./configs/db";
+
+const startServer = async (app: Express, port: number) => {
+  try {
+    connectToDatabase();
+    app.listen(port, () => {
+      console.log(`🚀 Server is up and running at: ${port}`);
+    });
+  } catch (error: any) {
+    console.error("❌ Failed to start the server:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer(app, Number(process.env.SERVER_PORT || 4000));
