81c84a29b1f87748d92e9265a97d4fe580715dd7
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-05-13 22:37:43 +0330

    Add product controller with create, get, update, and remove functions

---
+++ dist/Products/product.controller.js
@@ -0,0 +1,87 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.remove = exports.update = exports.getOne = exports.getAll = exports.createOne = void 0;
+const BaseProduct_1 = require("../models/BaseProduct");
+const FoodProducts_1 = __importDefault(require("../models/FoodProducts"));
+const DrinkProduct_1 = __importDefault(require("../models/DrinkProduct"));
+const createOne = async (req, res, next) => {
+    try {
+        const { name, price, type, description, isAvailable, ingredients, calories, weight, volume, caffeineLevel, } = req.body;
+        const existProduct = await BaseProduct_1.product.findOne({ name, price, type }).lean();
+        if (existProduct) {
+            res.status(409).json({ message: "این محصول از قبل وجود دارد." });
+            return;
+        }
+        if (type === "Food") {
+            await FoodProducts_1.default.create({
+                name,
+                price,
+                type,
+                description,
+                isAvailable,
+                ingredients,
+                calories,
+                weight,
+            });
+        }
+        else if (type === "Drink") {
+            await DrinkProduct_1.default.create({
+                name,
+                price,
+                type,
+                description,
+                isAvailable,
+                ingredients,
+                volume,
+                caffeineLevel,
+            });
+        }
+        else {
+            res
+                .status(409)
+                .json({ message: "لطفا نوع غذا یا نوشیدنی را مشخص کنید." });
+            return;
+        }
+        res.status(201).json({ message: "محصول مورد نظر با موفقیت ثبت گردید." });
+        return;
+    }
+    catch (error) {
+        next(error);
+    }
+};
+exports.createOne = createOne;
+const getAll = async (req, res, next) => {
+    try {
+    }
+    catch (error) {
+        next(error);
+    }
+};
+exports.getAll = getAll;
+const getOne = async (req, res, next) => {
+    try {
+    }
+    catch (error) {
+        next(error);
+    }
+};
+exports.getOne = getOne;
+const update = async (req, res, next) => {
+    try {
+    }
+    catch (error) {
+        next(error);
+    }
+};
+exports.update = update;
+const remove = async (req, res, next) => {
+    try {
+    }
+    catch (error) {
+        next(error);
+    }
+};
+exports.remove = remove;
