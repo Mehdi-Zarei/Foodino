328045554cfe2090e95d7960f4b2db0a79725ad0
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-05-13 22:36:35 +0330

    Add multer middleware and product routes to the Express app

--- dist/app.js
+++ dist/app.js
@@ -7,6 +7,12 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded());
+const multer_1 = __importDefault(require("multer")); //TODO
+app.use((0, multer_1.default)().none());
+//* Files Routes
+const product_routes_1 = __importDefault(require("./Products/product.routes"));
+//* Routes
+app.use("/product", product_routes_1.default);
 //* 404 Error Handler
 app.use((req, res) => {
     res.status(404).json({ message: "Oops!Page Not Found :(( " });
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
+import multer from "multer"; //TODO
+app.use(multer().none());
+
+//* Files Routes
+import productRouter from "./Products/product.routes";
+
+//* Routes
+app.use("/product", productRouter);
+
 //* 404 Error Handler
 app.use((req: Request, res: Response) => {
   res.status(404).json({ message: "Oops!Page Not Found :(( " });
