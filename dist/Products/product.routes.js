78755feff35e0c007480db4a4e4977233d539e5e
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-05-14 22:55:24 +0330

    Refactor product routes to include multer upload configuration for image handling

--- dist/Products/product.routes.js
+++ dist/Products/product.routes.js
@@ -1,8 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const product_controller_1 = require("./product.controller");
 const router = (0, express_1.Router)();
-router.route("/").post(product_controller_1.createOne).get(product_controller_1.getAll);
+const product_controller_1 = require("./product.controller");
+//* Uploader
+const multer_1 = require("../utils/multer");
+const upload = (0, multer_1.multerStorage)("public/images", 15, [".jpg", ".jpeg"]);
+//* Routes
+router.route("/").post(upload.array("images", 10), product_controller_1.createOne).get(product_controller_1.getAll);
 router.route("/:id").get(product_controller_1.getOne).patch(product_controller_1.update).delete(product_controller_1.remove);
 exports.default = router;
