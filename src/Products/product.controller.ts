e937923583cc2b0926583aea49a8066924d9736c
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-05-14 21:16:55 +0330

    Refactor product controller to use RequestHandler type and add mongoose import for object ID validation

--- src/Products/product.controller.ts
+++ src/Products/product.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction, RequestHandler } from "express";
 import { product } from "../models/BaseProduct";
 import foodModel from "../models/FoodProducts";
 import drinkModel from "../models/DrinkProduct";
+import mongoose from "mongoose";
 
 export const createOne: RequestHandler = async (req, res, next) => {
   try {
@@ -61,44 +62,53 @@ export const createOne: RequestHandler = async (req, res, next) => {
   }
 };
 
-export const getAll = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const getAll: RequestHandler = async (req, res, next) => {
+  try {
+    const products = await product.find({}).lean();
+
+    if (!product.length) {
+      res.status(404).json({ message: "هیچ محصولی پیدا نشد." });
+      return;
+    }
+    res.status(200).json(products);
+
+    return;
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getOne: RequestHandler = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.isValidObjectId(id)) {
+      res.status(409).json({ message: "آیدی وارد شده معتبر نمی باشد." });
+      return;
+    }
+
+    const mainProduct = await product.findById(id).lean();
+
+    if (!mainProduct) {
+      res.status(404).json({ message: "هیچ محصولی یافت نشد!" });
+      return;
+    }
+
+    res.status(200).json(mainProduct);
+    return;
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const update: RequestHandler = async (req, res, next) => {
   try {
   } catch (error) {
     next(error);
   }
 };
 
-export const getOne = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const update = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const remove = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const remove: RequestHandler = async (req, res, next) => {
   try {
   } catch (error) {
     next(error);
