da506c0eeeff99f0786c92d41dd347681480e05a
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-05-16 13:51:33 +0330

    Add image handling and update logic in product controller

--- dist/Products/product.controller.js
+++ dist/Products/product.controller.js
@@ -4,10 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.remove = exports.update = exports.getOne = exports.getAll = exports.createOne = void 0;
+const mongoose_1 = __importDefault(require("mongoose"));
 const BaseProduct_1 = require("../models/BaseProduct");
 const FoodProducts_1 = __importDefault(require("../models/FoodProducts"));
 const DrinkProduct_1 = __importDefault(require("../models/DrinkProduct"));
-const mongoose_1 = __importDefault(require("mongoose"));
 const createOne = async (req, res, next) => {
     try {
         const { name, price, type, description, isAvailable, ingredients, calories, weight, volume, caffeineLevel, } = req.body;
@@ -16,6 +16,7 @@ const createOne = async (req, res, next) => {
             res.status(409).json({ message: "این محصول از قبل وجود دارد." });
             return;
         }
+        const productImages = req.files?.map((img) => `public/images/${img.filename}`);
         if (type === "Food") {
             await FoodProducts_1.default.create({
                 name,
@@ -23,6 +24,7 @@ const createOne = async (req, res, next) => {
                 type,
                 description,
                 isAvailable,
+                images: productImages,
                 ingredients,
                 calories,
                 weight,
@@ -35,6 +37,7 @@ const createOne = async (req, res, next) => {
                 type,
                 description,
                 isAvailable,
+                images: productImages,
                 ingredients,
                 volume,
                 caffeineLevel,
@@ -91,6 +94,61 @@ const getOne = async (req, res, next) => {
 exports.getOne = getOne;
 const update = async (req, res, next) => {
     try {
+        const { id } = req.params;
+        const { name, price, type, description, isAvailable, ingredients, calories, weight, volume, caffeineLevel, } = req.body;
+        if (!mongoose_1.default.isValidObjectId(id)) {
+            res.status(409).json({ message: "آیدی وارد شده صحیح نمی باشد." });
+            return;
+        }
+        const mainProduct = await BaseProduct_1.product.findOne({ _id: id });
+        if (!mainProduct) {
+            res.status(404).json({ message: "محصولی برای آپدیت یافت نشد." });
+            return;
+        }
+        if (req.files) {
+        }
+        if (name)
+            mainProduct.name = name;
+        if (price)
+            mainProduct.price = price;
+        if (type)
+            mainProduct.type = type;
+        if (description)
+            mainProduct.description = description;
+        if (isAvailable)
+            mainProduct.isAvailable = isAvailable;
+        if (ingredients)
+            mainProduct.ingredients = ingredients;
+        await mainProduct.save();
+        switch (mainProduct.__t) {
+            case "FoodProduct": {
+                const food = await FoodProducts_1.default.findById(id);
+                if (!food)
+                    break;
+                if (calories)
+                    food.calories = calories;
+                if (weight)
+                    food.weight = weight;
+                await food.save();
+                break;
+            }
+            case "DrinkProduct": {
+                const drink = await DrinkProduct_1.default.findById(id);
+                if (!drink)
+                    break;
+                if (volume)
+                    drink.volume = volume;
+                if (caffeineLevel)
+                    drink.caffeineLevel = caffeineLevel;
+                await drink.save();
+                break;
+            }
+            default:
+                res.status(400).json({ message: "نوع محصول ناشناخته است." });
+                break;
+        }
+        res.status(200).json({ message: "محصول مورد نظر با موفقیت آپدیت شد." });
+        return;
     }
     catch (error) {
         next(error);
