15c8b4eb0c27b41478c1118262eb5bcb255855c2
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-05-16 13:51:44 +0330

    Refactor product routes to improve readability and maintainability

--- dist/Products/product.routes.js
+++ dist/Products/product.routes.js
@@ -8,5 +8,9 @@ const multer_1 = require("../utils/multer");
 const upload = (0, multer_1.multerStorage)("public/images", 15, [".jpg", ".jpeg"]);
 //* Routes
 router.route("/").post(upload.array("images", 10), product_controller_1.createOne).get(product_controller_1.getAll);
-router.route("/:id").get(product_controller_1.getOne).patch(product_controller_1.update).delete(product_controller_1.remove);
+router
+    .route("/:id")
+    .get(product_controller_1.getOne)
+    .patch(upload.array("images", 10), product_controller_1.update)
+    .delete(product_controller_1.remove);
 exports.default = router;
