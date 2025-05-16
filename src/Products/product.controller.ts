aa2a0f125a063949ac3d22f7b675542e105e47e1
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-05-16 13:51:57 +0330

    Add image handling and update logic in product controller

--- src/Products/product.controller.ts
+++ src/Products/product.controller.ts
@@ -1,8 +1,8 @@
 import { RequestHandler } from "express";
+import mongoose from "mongoose";
 import { product } from "../models/BaseProduct";
 import foodModel from "../models/FoodProducts";
 import drinkModel from "../models/DrinkProduct";
-import mongoose from "mongoose";
 
 export const createOne: RequestHandler = async (req, res, next) => {
   try {
@@ -26,6 +26,10 @@ export const createOne: RequestHandler = async (req, res, next) => {
       return;
     }
 
+    const productImages = (req.files as Express.Multer.File[])?.map(
+      (img) => `public/images/${img.filename}`
+    );
+
     if (type === "Food") {
       await foodModel.create({
         name,
@@ -33,6 +37,7 @@ export const createOne: RequestHandler = async (req, res, next) => {
         type,
         description,
         isAvailable,
+        images: productImages,
         ingredients,
         calories,
         weight,
@@ -44,6 +49,7 @@ export const createOne: RequestHandler = async (req, res, next) => {
         type,
         description,
         isAvailable,
+        images: productImages,
         ingredients,
         volume,
         caffeineLevel,
@@ -103,6 +109,72 @@ export const getOne: RequestHandler = async (req, res, next) => {
 
 export const update: RequestHandler = async (req, res, next) => {
   try {
+    const { id } = req.params;
+    const {
+      name,
+      price,
+      type,
+      description,
+      isAvailable,
+      ingredients,
+      calories,
+      weight,
+      volume,
+      caffeineLevel,
+    } = req.body;
+
+    if (!mongoose.isValidObjectId(id)) {
+      res.status(409).json({ message: "آیدی وارد شده صحیح نمی باشد." });
+      return;
+    }
+
+    const mainProduct = await product.findOne({ _id: id });
+
+    if (!mainProduct) {
+      res.status(404).json({ message: "محصولی برای آپدیت یافت نشد." });
+      return;
+    }
+
+    if (req.files) {
+    }
+
+    if (name) mainProduct.name = name;
+    if (price) mainProduct.price = price;
+    if (type) mainProduct.type = type;
+    if (description) mainProduct.description = description;
+    if (isAvailable) mainProduct.isAvailable = isAvailable;
+    if (ingredients) mainProduct.ingredients = ingredients;
+
+    await mainProduct.save();
+
+    switch (mainProduct.__t) {
+      case "FoodProduct": {
+        const food = await foodModel.findById(id);
+        if (!food) break;
+
+        if (calories) food.calories = calories;
+        if (weight) food.weight = weight;
+
+        await food.save();
+        break;
+      }
+      case "DrinkProduct": {
+        const drink = await drinkModel.findById(id);
+        if (!drink) break;
+
+        if (volume) drink.volume = volume;
+        if (caffeineLevel) drink.caffeineLevel = caffeineLevel;
+
+        await drink.save();
+        break;
+      }
+      default:
+        res.status(400).json({ message: "نوع محصول ناشناخته است." });
+        break;
+    }
+
+    res.status(200).json({ message: "محصول مورد نظر با موفقیت آپدیت شد." });
+    return;
   } catch (error) {
     next(error);
   }
