589bbc44ca3c7f25cf4533b090ca555b8d5ec9f9
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-05-17 22:01:27 +0330

    Add cart model with schema and pre-save total price calculation

---
+++ src/models/Cart.ts
@@ -0,0 +1,58 @@
+import mongoose, { Types } from "mongoose";
+
+interface ICartItems {
+  product: Types.ObjectId;
+  quantity: number;
+  priceAtTimeOfPurchase: number;
+}
+
+interface ICart {
+  user: Types.ObjectId;
+  items: ICartItems[];
+  totalCartPrice: number;
+}
+
+const cartItemsSchema = new mongoose.Schema<ICartItems>(
+  {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    priceAtTimeOfPurchase: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const cartSchema = new mongoose.Schema<ICart>(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [cartItemsSchema],
+    totalCartPrice: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+const model = mongoose.model<ICart>("Cart", cartSchema);
+
+cartSchema.pre("save", function (next) {
+  this.totalCartPrice = this.items.reduce((total, item) => {
+    return total + item.priceAtTimeOfPurchase * item.quantity;
+  }, 0);
+  next();
+});
+
+export { model as cartModel };
