a62c757a429534ffdf0fd8a0b4b2850d52593773
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-05-19 19:35:50 +0330

    Refactor routes in app.js and app.ts to include API prefix and improve middleware organization

--- dist/app.js
+++ dist/app.js
@@ -4,22 +4,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
+const path_1 = __importDefault(require("path"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const Error_Handler_1 = require("./Middlewares/Error Handler");
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use("/public", express_1.default.static(path_1.default.join(__dirname, "..", "public")));
+app.use((0, cookie_parser_1.default)());
 //* Files Routes
 const product_routes_1 = __importDefault(require("./Products/product.routes"));
 const auth_routes_1 = __importDefault(require("./Auth/auth.routes"));
 const user_routes_1 = __importDefault(require("./User/user.routes"));
 const cart_routes_1 = __importDefault(require("./Cart/cart.routes"));
-const Error_Handler_1 = require("./Middlewares/Error Handler");
+const checkout_routes_1 = __importDefault(require("./Checkout/checkout.routes"));
 //* Routes
-app.use("/product", product_routes_1.default);
-app.use("/auth", auth_routes_1.default);
-app.use("/user/cart", cart_routes_1.default);
-app.use("/user", user_routes_1.default);
+app.use("/api/product", product_routes_1.default);
+app.use("/api/auth", auth_routes_1.default);
+app.use("/api/user/cart", cart_routes_1.default);
+app.use("/api/user", user_routes_1.default);
+app.use("/api/checkouts", checkout_routes_1.default);
 //* 404 Error Handler
 app.use((req, res) => {
     res.status(404).json({ message: "Oops!Page Not Found :(( " });
--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,27 @@
 import express, { Request, Response } from "express";
-import path from "path";
 const app = express();
+import path from "path";
+import cookieParser from "cookie-parser";
+import { errorHandler } from "./Middlewares/Error Handler";
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/public", express.static(path.join(__dirname, "..", "public")));
+app.use(cookieParser());
 
 //* Files Routes
 import productRouter from "./Products/product.routes";
 import authRouter from "./Auth/auth.routes";
 import userRouter from "./User/user.routes";
 import cartRouter from "./Cart/cart.routes";
-import { errorHandler } from "./Middlewares/Error Handler";
+import checkoutRouter from "./Checkout/checkout.routes";
 
 //* Routes
-app.use("/product", productRouter);
-app.use("/auth", authRouter);
-app.use("/user/cart", cartRouter);
-app.use("/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/user/cart", cartRouter);
+app.use("/api/user", userRouter);
+app.use("/api/checkouts", checkoutRouter);
 
 //* 404 Error Handler
 app.use((req: Request, res: Response) => {
